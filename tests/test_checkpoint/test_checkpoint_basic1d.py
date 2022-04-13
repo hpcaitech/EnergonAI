d5120322b81740752e57f3fbab2c2c1f0559c7dc
maruyama <china6280111@126.com>
2022-04-13 10:48:48 +0800

    delete pipeline.py

--- tests/test_checkpoint/test_checkpoint_basic1d.py
+++ tests/test_checkpoint/test_checkpoint_basic1d.py
@@ -3,6 +3,7 @@
 
 import pprint
 from functools import partial
+from energon.logging import get_dist_logger
 
 import energon.nn as col_nn
 import pytest
@@ -16,9 +17,30 @@ from energon.logging import disable_existing_loggers
 from energon.utils import free_port, is_using_pp
 from energon.utils.checkpointing import gather_pipeline_parallel_state_dict, load_checkpoint, save_checkpoint
 
+def partition_uniform(num_items, pipeline_parallel_size, num_chunks):
+    assert num_items % num_chunks == 0, \
+        "Layer length should be divided by the number of chunks, otherwise parameter method is recomended"
+
+    logger = get_dist_logger()
+    parts = [[] for _ in range(pipeline_parallel_size)]
+    partition_items = num_items // num_chunks
+    for idx in range(num_chunks):
+        base_idx = idx * partition_items
+        chunk_size = partition_items // pipeline_parallel_size
+        left = pipeline_parallel_size - partition_items % pipeline_parallel_size
+        if chunk_size == 0:
+            logger.warning("Some nodes in Pipeline have no requests")
+
+        for p in range(pipeline_parallel_size):
+            st = base_idx
+            base_idx += chunk_size + (p >= left)
+            parts[p].append((st, base_idx))
+
+    return parts
+
 
 def build_pipeline(model):
-    from energon.builder.pipeline import partition_uniform
+
 
     pipeline_size = gpc.get_world_size(ParallelMode.PIPELINE)
     pipeline_rank = gpc.get_local_rank(ParallelMode.PIPELINE)

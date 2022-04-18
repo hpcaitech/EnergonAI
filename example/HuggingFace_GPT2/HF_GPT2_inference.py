6d52b7dc277af27a4b944d33d643cb81ee9de914
maruyama <china6280111@126.com>
2022-04-18 15:11:24 +0800

    adding batch

--- example/HuggingFace_GPT2/HF_GPT2_inference.py
+++ example/HuggingFace_GPT2/HF_GPT2_inference.py
@@ -4,6 +4,8 @@ import torch
 import torch.multiprocessing as mp
 import torch.nn as nn
 
+from transformers import GPT2Tokenizer
+
 from energon.context import ParallelMode
 from energon.core import global_context as gpc
 
@@ -44,6 +46,7 @@ def build_gpt_model():
                     "prefix": "",
                     "vocab_size": 50257,
                     "HuggingFace": True}
+    print("here")
     engine = InferenceEngine(model_class=gpt2_small,
                              model_config=model_config,
                              model_type='gpt',
@@ -52,25 +55,44 @@ def build_gpt_model():
                              pp_init_size=args.pipe_para_size,
                              port=args.port,
                              dtype=dtype_)
-    input_ids = torch.randint(1, 10, (32, 40), dtype=torch.int64)
-    attention_mask = torch.randint(0, 1, (32, 1, 40), dtype=torch.int64)
+    print("pe")
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    test_input = ["MANY YEARS LATER as he faced the firing squad, Colonel Aureliano Buendía was to remember that"
+                  for _ in range(10)]
+    input_token = tokenizer(test_input, return_tensors='pt')
+    # print(input_ids['input_ids'])
+    # print(input_ids['attention_mask'])
+    # input_ids = input_token['input_ids']
+    # print(input_ids.shape)
+    # input_ids = torch.randint(1, 10, (32, 40), dtype=torch.int64)
+    # attention_mask = torch.randint(0, 1, (32, 1, 40), dtype=torch.int64)
     hidden_states = None
-    sample = dict(hidden_states=hidden_states, input_ids=input_ids, attention_mask=attention_mask)
+    # sample = dict(hidden_states=hidden_states, input_ids=input_ids, attention_mask=attention_mask)
+    # sample = dict(hidden_states=hidden_states, input_ids=input_ids)
 
-    output = engine.run(sample)
+    output = engine.run(input_token)
     # print(output.to_here())
     output.to_here()
+    print(tokenizer.decode(output))
     timer = get_timers()
     timer('time1').start()
 
     for i in range(1, args.iteration):
-        input_ids = torch.randint(1, 10, (32, i % 20 + 2), dtype=torch.int64)
-        attention_mask = torch.randint(0, 1, (32, 1, i % 20 + 2), dtype=torch.int64)
+        input_ids = tokenizer(test_input, return_tensors='pt')
+        # input_ids = torch.randint(1, 10, (32, 40), dtype=torch.int64)
+        # attention_mask = torch.randint(0, 1, (32, 1, 40), dtype=torch.int64)
         hidden_states = None
-        sample = dict(hidden_states=hidden_states, input_ids=input_ids, attention_mask=attention_mask)
-        output = engine.run(sample)
+        # sample = dict(hidden_states=hidden_states, input_ids=input_ids, attention_mask=attention_mask)
+        # sample = dict(hidden_states=hidden_states, input_ids=input_ids)
+        # input_ids = torch.randint(1, 10, (32, i % 20 + 2), dtype=torch.int64)
+        # attention_mask = torch.randint(0, 1, (32, 1, i % 20 + 2), dtype=torch.int64)
+        # hidden_states = None
+        # sample = dict(hidden_states=hidden_states, input_ids=input_ids, attention_mask=attention_mask)
+        output = engine.run(input_ids)
+        output.to_here()
+        print(tokenizer.decode(output))
 
-    print(output.to_here())
+    # print(output.to_here())
     timer('time1').stop()
 
     time1 = timer('time1').elapsed()

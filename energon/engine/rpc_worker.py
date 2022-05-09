cb1b53aa73e5b66f6b0824960fd2b05325073776
maruyama <china6280111@126.com>
2022-05-09 16:11:56 +0800

    new branch

--- energon/engine/rpc_worker.py
+++ energon/engine/rpc_worker.py
@@ -15,26 +15,28 @@ WRAPPER_TYPES = {
     "bert": BertPipelineCommWrapper,
 }
 
+
 class ReturnDict:
     def __init__(self):
         self.rd = dict()
-    
+
     def enqueue(self, key, output):
         self.rd[key] = output
-    
+
     def top(self, key):
         while key not in self.rd:
             time.sleep(0.001)
         output = self.rd.pop(key)
         return output
 
+
 class RPCWorker:
-    def __init__(self, 
-                 model_class, 
+    def __init__(self,
+                 model_class,
                  model_config,
                  model_type,
                  dtype,
-                 max_batch_size:int  = 1) -> None:
+                 max_batch_size: int = 1) -> None:
         self.model_class = model_class
         self.model_config = model_config
         self.dtype = dtype
@@ -42,9 +44,9 @@ class RPCWorker:
         self.pipe_wrapper = WRAPPER_TYPES[model_type]
 
         self.WORKER_NAME = "wok{}"
-        self.model = None # call the model
+        self.model = None  # call the model
         self.rank = gpc.get_local_rank(ParallelMode.GLOBAL)
-        
+
         torch.cuda.set_device(f'cuda:{gpc.get_local_rank(ParallelMode.GLOBAL)}')
         self._init_self()
 
@@ -56,27 +58,25 @@ class RPCWorker:
         if self.dtype == torch.half:
             self.model = self.model_class(**self.model_config).cuda().half()
         else:
-            self.model = self.model_class(**self.model_config).cuda()      
-        # print("Pass")
+            self.model = self.model_class(**self.model_config).cuda()
+            # print("Pass")
         self.model.eval()
 
-
-        self.model = self.pipe_wrapper(model = self.model, max_batch_size = self.max_batch_size, dtype=self.dtype)  
+        self.model = self.pipe_wrapper(model=self.model, max_batch_size=self.max_batch_size, dtype=self.dtype)
 
     def run(self, key, inputs):
         # print("key: {}".format(key), flush=True)
-        torch.cuda.set_device(f'cuda:{gpc.get_local_rank(ParallelMode.GLOBAL)}') 
+        torch.cuda.set_device(f'cuda:{gpc.get_local_rank(ParallelMode.GLOBAL)}')
         for k, v in inputs.items():
             if v is not None:
-                inputs[k] = v.cuda() #non_blocking=True
+                inputs[k] = v.cuda()  # non_blocking=True
 
-                
         if (gpc.is_initialized(ParallelMode.PIPELINE)) and (not gpc.is_last_rank(ParallelMode.PIPELINE)):
             self.model.run(key, inputs)
             return None
-        else: 
+        else:
             output, cur_key = self.model.run(key, inputs)
             self.return_dict.enqueue(cur_key, output.cpu())
             return self.return_dict.top(key)
-            
+
         return None

4be7b35ff7d8894ba2a49ea2aef19661abeb03fe
maruyama <china6280111@126.com>
2022-05-26 10:48:54 +0800

    new version of batch manager

--- examples/gpt/gpt_config.py
+++ examples/gpt/gpt_config.py
@@ -2,7 +2,7 @@ from gpt import gpt2_small, gpt2_medium, gpt2_large, gpt2_xl, gpt2_8B, gpt3
 from gpt_batch_server import launch_engine
 
 # for engine
-model_class = gpt2_large
+model_class = gpt2_8B
 model_type = "gpt"
 host = "127.0.0.1"
 port = 29401
@@ -16,7 +16,7 @@ pp_init_size = 2
 # for server
 engine_server = launch_engine
 server_host = "127.0.0.1"
-server_port = 8020
+server_port = 8016
 log_level = "info"
 tokenizer_path = "/home/lcdjs/hf_gpt2"
 rm_padding = False

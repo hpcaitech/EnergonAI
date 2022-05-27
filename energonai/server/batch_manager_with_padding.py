90fe8ba1fc34cd5f69b6ffe4bb030286c7778b84
maruyama <china6280111@126.com>
2022-05-27 14:42:44 +0800

    refactor batch managers

--- energonai/server/batch_manager_with_padding.py
+++ energonai/server/batch_manager_with_padding.py
@@ -9,7 +9,6 @@ from collections import deque
 import torch.cuda
 import numpy as np
 import scipy.stats as stats
-from energonai.engine import InferenceEngine
 import random
 import redis
 import math
@@ -19,21 +18,22 @@ import threading
 from readerwriterlock import rwlock
 import logging
 from concurrent.futures import ThreadPoolExecutor
+from energonai.context import mcfg
 
 class gamma_dist:
-    def __init__(self, alpha_, loc_, beta_, max_list_len, max_seq_len):
+    def __init__(self, alpha_, loc_, beta_):
         self.alpha = alpha_
         self.loc = loc_
         self.beta = beta_
-        self.max_list_len = max_list_len
-        self.max_seq_len = max_seq_len
+        self.max_list_len = 5 * mcfg['max_batch_size']
+        self.max_seq_len = mcfg['max_sequence_length']
 
     def complete_req_list(self, req_list):
         new_size = self.max_list_len - len(req_list)
         res = stats.gamma.rvs(self.alpha, loc=self.loc, scale=self.beta, size=new_size)
         res = [math.floor(i) + 1 for i in res]
         res = [i if i < self.max_seq_len else self.max_seq_len for i in res]
-        new_req = [single_request(None, None, None, i) for i in res]
+        new_req = [single_request(input_=None, time_stamp=None, seq_len=i) for i in res]
         new_req.extend(req_list)
         new_req.sort(key=lambda x: x.seq_len)
         return new_req
@@ -41,21 +41,24 @@ class gamma_dist:
 
 class single_request:
 
-    def __init__(self, input_, time_stamp, input_str, seq_len : int=0):
+    def __init__(self, input_, time_stamp, **kwargs):
         """
         class to store related information for a single request.
         :param input_: The output of GPT2Tokenizer.tokenizer, a dict including input_ids and attention_mask
         :param time_stamp: The time stamp when we receive the request. We use the time stamp as a index to
                             identify the request.
-        :param input_str: The input string of the request.
         """
         self.input_ = input_
-        self.text = input_str
         self.time_ = time_stamp
-        if input_:
-            self.seq_len = input_['input_ids'].shape[1]
+        if 'seq_len' not in kwargs.keys():
+            if mcfg['model_type'] in ['gpt', 'bert']:
+                self.seq_len = input_['input_ids'].shape[1]
+            elif mcfg['model_type'] == 'vit':
+                self.seq_len = input_.shape[-1]
         else:
-            self.seq_len = seq_len
+            self.seq_len = kwargs['seq_len']
+        if 'input_str' in kwargs.keys():
+            self.text = kwargs['input_str']
 
 
 class Manager:
@@ -66,7 +69,7 @@ class Manager:
     def __init__(self):
         pass
 
-    def insert_req(self, time_stamp: float, input_ids, input_str: str):
+    def insert_req(self, time_stamp: float, input_ids):
         pass
 
 
@@ -78,82 +81,39 @@ class new_Batch_Manager(Manager):
     """
 
     def __init__(self,
-                 engine: InferenceEngine,
-                 model_name: str,
-                 pp: int,
-                 tp: int,
-                 max_sequence_length: int,
-                 max_batch_size: int = 32,
-                 tokenizer=None,
-                 pad_token=None,
-                 rm_padding=False,
+                 forward_func,
+                 result_process,
                  load_history=False,
-                 step: int = 1,
-                 repeat_round: int = 3,
-                 max_wait_time=1,
-                 his_len: int = 300):
-        """
-        :param engine: The InferenceEngine from energon.engine
-        :param cached_cost: The output of function generate_cached_cost
-        :param init_mu: initial mean value we suppose for incoming sequence length.
-        :param init_theta: initial variance value we suppose for incoming sequence length.
-        :param max_batch_size: the max number of requests that can be wrapped into one batch.
-        :param lr: the learning rate we use to update the mean and variance that we suppose for the normal
-                    distribution of sequence length.
-        """
+                 his_len: int = 300, **kwargs):
         super().__init__()
-        self.engine = engine
-        self.max_batch_size = max_batch_size
+        self.max_batch_size = mcfg['max_batch_size']
+        self.max_sequence_length = mcfg['max_sequence_length']
         if load_history:
-            self.req_history = self.load_history(his_len)
+            self.load_history(his_len)
         else:
             self.req_history = deque(maxlen=his_len)
-        self.max_wait_time = max_wait_time
         self.req_list = []
         self.req_list_lock = rwlock.RWLockFair()
-        self.max_his_length = his_len
-        self.gamma_dist_ = self.init_gamma_dist(max_sequence_length)
         self.write_lock = self.req_list_lock.gen_wlock()
-        self.cached_cost = self.generate_cached_cost(engine,
-                                                     model_name,
-                                                     pp=pp,
-                                                     tp=tp,
-                                                     max_seq_len=max_sequence_length,
-                                                     max_batch_size=max_batch_size,
-                                                     step=step,
-                                                     repeat_round=repeat_round,
-                                                     tokenizer=tokenizer)
-        self.tokenizer = tokenizer
-        self.rm_padding = rm_padding
-        if self.tokenizer and pad_token:
-            self.tokenizer.pad_token = pad_token  # GPT2Tokenizer.eos_token
+        self.max_his_length = his_len
+        self.gamma_dist_ = self.init_gamma_dist(self.max_sequence_length)
+        self.cached_cost = self.generate_cached_cost()
         self.running_flag = True
         self.publisher = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
-        self.pool = ThreadPoolExecutor(max_workers=pp + 1)
+        self.pool = ThreadPoolExecutor(max_workers=mcfg['pp_init_size'] + 1)
         self.main_thread = threading.Thread(target=self.processing_batch)
+        self.forward_func = forward_func
+        self.result_process = result_process
         self.main_thread.start()
 
     def init_gamma_dist(self, max_seq_len):
         if len(self.req_history) == 0:
-            return gamma_dist(alpha_=0.022, loc_=11, beta_=3.34,
-                              max_list_len=5 * self.max_batch_size,
-                              max_seq_len=max_seq_len)
+            return gamma_dist(alpha_=0.022, loc_=11, beta_=3.34,)
         else:
             fit_alpha, fit_loc, fit_beta = stats.gamma.fit(self.req_history)
-            return gamma_dist(alpha_=fit_alpha, loc_=fit_loc, beta_=fit_beta,
-                              max_list_len=5 * self.max_batch_size,
-                              max_seq_len=max_seq_len)
+            return gamma_dist(alpha_=fit_alpha, loc_=fit_loc, beta_=fit_beta)
 
-    def generate_cached_cost(self,
-                             engine,
-                             model_name: str,
-                             pp: int,
-                             tp: int,
-                             max_seq_len: int = 1024,
-                             max_batch_size: int = 16,
-                             step: int = 1,
-                             repeat_round: int = 3,
-                             tokenizer=None):
+    def generate_cached_cost(self):
         """
         Test the running time for different sequence length and batch size on the current machine.
         :param engine: InferenceEngine from energon.engine
@@ -164,48 +124,33 @@ class new_Batch_Manager(Manager):
         :param repeat_round: We inference current batch 'repeat_round' times and take average.
         """
         logging.log(0, "fetching cached cost")
-        cached_name = "cached_cost_{}_pp{}_tp{}_{}_{}_{}_{}.npy".format(model_name, pp, tp, max_seq_len, max_batch_size,
-                                                                        step, repeat_round)
+        cached_name = "cached_cost_{}_pp{}_tp{}_{}_{}_{}_{}.npy"\
+            .format(mcfg['model_name'], mcfg['pp_init_size'], mcfg['tp_init_size'],
+                    mcfg['max_sequence_length'], mcfg['max_batch_size'],
+                    mcfg['step'], mcfg['repeat_round'])
         if os.path.exists(cached_name):
             logging.log(0, "loading cached cost from file")
             cached_cost = np.load(cached_name).tolist()
         else:
             logging.log(0, "generating new cached cost")
-            cached_cost = [[0 for i in range(max_batch_size + 1)] for j in range(max_seq_len + 1)]
-            warm_up_str = "test test test"
-            if tokenizer:
-                warm_up_input = tokenizer(warm_up_str, return_tensors="pt")
-            else:
-                warm_up_input = warm_up_str
+            cached_cost = [[0 for i in range(mcfg['max_batch_size'] + 1)] for j in range(mcfg['max_sequence_length'] + 1)]
             for tt in range(5):
-                output = engine.run(warm_up_input)
-                predictions = output.to_here()
+                output_ = self.forward_func(mcfg['max_sequence_length'] - 1, mcfg['max_batch_size'] - 1)
+                output_.to_here()
+                result = self.result_process(output_)
             input_text = ""
-            for tmp_len in trange(1, max_seq_len + 1, step):
+            for tmp_len in trange(1, mcfg['max_sequence_length'] + 1, mcfg['step']):
                 input_text += "test "
-                for tmp_batch in range(1, max_batch_size + 1):
-                    batched_text = [input_text for _ in range(tmp_batch)]
-                    if tokenizer:
-                        input_token = tokenizer(batched_text, return_tensors="pt")
-                    else:
-                        input_token = batched_text
-                    output = engine.run(input_token)
-                    predictions = output.to_here()
-                    if tokenizer:
-                        tokenizer.decode(predictions)
+                for tmp_batch in range(1, mcfg['max_batch_size'] + 1):
+                    self.forward_func(tmp_len, tmp_batch)
                     start_time = time.time()
-                    for k in range(repeat_round):
-                        if tokenizer:
-                            input_token = tokenizer(batched_text, return_tensors="pt")
-                        else:
-                            input_token = batched_text
-                        output = engine.run(input_token)
-                        predictions = output.to_here()
-                        if tokenizer:
-                            tokenizer.decode(predictions)
-                    time_cost = (time.time() - start_time) / repeat_round
+                    for k in range(mcfg['repeat_round']):
+                        output_ = self.forward_func(tmp_len, tmp_batch)
+                        output_.to_here()
+                        result = self.result_process(output_)
+                    time_cost = (time.time() - start_time) / mcfg['repeat_round']
                     cached_cost[tmp_len][tmp_batch] = time_cost
-                    for k in range(1, step):
+                    for k in range(1, mcfg['step']):
                         cached_cost[tmp_len + k][tmp_batch] = time_cost
             np.save(cached_name, np.array(cached_cost))
         logging.log(0, "cached cost loaded")
@@ -235,11 +180,11 @@ class new_Batch_Manager(Manager):
                     break
         return predictions
 
-    def insert_req(self, time_stamp: float, input_ids, input_str: str):
+    def insert_req(self, time_stamp: float, input_ids):
         """
         Build a single_request class with the input string and then insert it into the queue.
         """
-        tmp_req = single_request(input_ids, time_stamp, input_str)
+        tmp_req = single_request(input_ids, time_stamp)
         self.write_lock.acquire()
         self.req_list.append(tmp_req)
         self.write_lock.release()
@@ -252,7 +197,6 @@ class new_Batch_Manager(Manager):
         """
         self.write_lock.acquire()
         new_req_list = self.gamma_dist_.complete_req_list(self.req_list)
-        print("befor req num: {}".format(len(self.req_list)))
         self.req_list = []
         self.write_lock.release()
         states = [0]
@@ -293,21 +237,17 @@ class new_Batch_Manager(Manager):
         for req in temp_batch:
             if req.input_:
                 result_batch.append(req)
-        print("a fake batch of {} is wrapped".format(len(temp_batch)))
-        print("a batch of {} req is wrapped".format(len(result_batch)))
         self.write_lock.acquire()
-        print("meanwhile, {} new req came".format(len(self.req_list)))
         for req_ in new_req_list:
             if req_.input_:
                 self.req_list.append(req_)
-        print("after, {} req is left".format(len(self.req_list)))
         self.write_lock.release()
         return result_batch
 
     def cal_priority(self, batch, cur_time):
         completeness = np.sum([1 if i.input_ else 0 for i in batch]) / len(batch)
         earliest_time_stamp = min([j.time_ if j.time_ else cur_time for j in batch])
-        if cur_time - earliest_time_stamp > self.max_wait_time:
+        if cur_time - earliest_time_stamp > mcfg['max_wait_time']:
             completeness = 1.1
         return completeness
 
@@ -332,18 +272,8 @@ class new_Batch_Manager(Manager):
                 logging.info("A batch with {} requests and length of {} packed, in-batch length: {}".format(
                     len(target_batch), pad_len, [p.seq_len for p in target_batch]))
                 input_text = [i.text for i in target_batch]
-                if self.tokenizer:
-                    input_ids = self.tokenizer(input_text, padding="longest", return_tensors="pt")
-                else:
-                    input_ids = input_text
-                if self.rm_padding:
-                    input_ids['seq_lens'] = torch.Tensor([p.seq_len for p in target_batch])
-                output = self.engine.run(input_ids)
-                self.pool.submit(self.publish_result, output, target_batch)
-                # self.update_norm(target_batch)
-                # self.publish_result(output, target_batch, start_time)
-                # pub_thread = threading.Thread(target=self.publish_result, args=(output, target_batch, start_time))
-                # pub_thread.start()
+                output_ = self.forward_func(input_text)
+                self.pool.submit(self.publish_result, output_, target_batch)
                 if round_cnt == 10 and len(self.req_history) >= self.max_his_length - 1:
                     round_cnt = 0
                     self.update_distribution()
@@ -361,8 +291,5 @@ class new_Batch_Manager(Manager):
         for i in range(len(target_batch)):
             temp_st = target_batch[i].time_
             chosen_pred = predictions[i]
-            if self.tokenizer:
-                text_ = self.tokenizer.decode(int(chosen_pred))
-            else:
-                text_ = chosen_pred
-            self.publisher.publish(str(temp_st), text_)
+            result = self.result_process(chosen_pred)
+            self.publisher.publish(str(temp_st), result)

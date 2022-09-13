691e3457fb747ccedef1734d9a82b0bc8fb9fffa
ver217 <lhx0217@gmail.com>
2022-09-13 17:53:53 +0800

    fix standalone script

--- tests/run_standalone_tests.sh
+++ tests/run_standalone_tests.sh
@@ -3,6 +3,5 @@ set -e
 
 export PYTHONPATH=$(realpath $(dirname $0))
 
-# find . -name "test*.py" -print0 | xargs -0L1 pytest -m "standalone"
-find test_engine -name "test*.py" -print0 | xargs -0L1 pytest -m "standalone"
+find . -name "test*.py" -print0 | xargs -0L1 pytest -m "standalone"
 
